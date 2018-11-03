2fcfbff6fa5bd0a4a6b97da2726008729c7cc622
laptop <lukaszprokopiuk@gmail.com>
2018-11-03 19:27:20 +0100

    Updates

---
+++ precache-manifest.a6a97143713dbf862df95e8b60ac3f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e911c30c7f012f0ebc7",
+    "url": "/remakeGame2018/static/js/runtime~main.1e911c30.js"
+  },
+  {
+    "revision": "6899cbc7962f8533c5de",
+    "url": "/remakeGame2018/static/js/main.6899cbc7.chunk.js"
+  },
+  {
+    "revision": "30a3e0cc12a958f4b072",
+    "url": "/remakeGame2018/static/js/1.30a3e0cc.chunk.js"
+  },
+  {
+    "revision": "6899cbc7962f8533c5de",
+    "url": "/remakeGame2018/static/css/main.658792e6.chunk.css"
+  },
+  {
+    "revision": "641b03d74d3164ef7756367cd1510486",
+    "url": "/remakeGame2018/index.html"
+  }
+];
